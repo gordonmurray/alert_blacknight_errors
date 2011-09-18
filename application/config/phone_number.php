e32c77f582e581459bd5b065c9aec8f67dcc88dd
Gordon Murray <gordon@murrion.com>
2011-09-18 17:05:15 +0100

    Updating Phone details

--- application/config/phone_number.php
+++ application/config/phone_number.php
@@ -3,8 +3,8 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-$config['phone_sender'] = '+353876228708';
-$config['phone_recipient'] = '+353876228708';
+$config['phone_sender'] = '';
+$config['phone_recipient'] = '';
 
 
 /* End of file phone_number.php */
